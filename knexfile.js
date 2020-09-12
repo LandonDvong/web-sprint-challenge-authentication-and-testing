9fdc8f92f12eb5413c2e058b122ef92fbc04c8d4
thaveesit douangaphaivong <d15.landon@gmail.com>
2020-09-12 12:54:36 -0500

    dependencies and knex setup

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,15 @@
+// Update with your config settings.
+
 module.exports = {
-  development: {
+
+  testing: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: { filename: './database/test.db3' },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
-  },
+  }
 };
